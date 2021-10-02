505d22f90ba6a025bb4f5356df2bf59ce11f91c5
willian95 <rodriguezwillian95@gmail.com>
2021-10-02 15:36:00 -0400

    listado rep

--- app/Console/Commands/RepJobExport.php
+++ app/Console/Commands/RepJobExport.php
@@ -96,10 +96,10 @@ class RepJobExport extends Command
         $data = ["url" => $url];
         $to_email = $email;
 
-        dump(env("MAIL_USERNAME"), env("MAIL_PASSWORD"), $to_email);
+       
 
         \Mail::send("emails.sendREP", $data, function($message) use ($to_email) {
-
+            dump(env("MAIL_USERNAME"), env("MAIL_PASSWORD"), $to_email);
             $message->to($to_email, "Usuario")->subject("¡Tu archivo está listo!");
             $message->from(env("MAIL_FROM_ADDRESS"), env("MAIL_FROM_NAME"));
 
