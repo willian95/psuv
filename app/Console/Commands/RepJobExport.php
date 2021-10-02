50c51ab222138313fdf406c9565d75c4ddec2e10
willian95 <rodriguezwillian95@gmail.com>
2021-10-02 15:27:19 -0400

    listado rep

--- app/Console/Commands/RepJobExport.php
+++ app/Console/Commands/RepJobExport.php
@@ -100,7 +100,7 @@ class RepJobExport extends Command
 
         \Mail::send("emails.sendREP", $data, function($message) use ($to_email) {
 
-            $message->to($to_email)->subject("¡Tu archivo está listo!");
+            $message->to($to_email, "Usuario")->subject("¡Tu archivo está listo!");
             $message->from(env("MAIL_FROM_ADDRESS"), env("MAIL_FROM_NAME"));
 
         });
