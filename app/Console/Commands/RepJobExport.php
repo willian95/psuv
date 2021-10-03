cb6102d7bd47255b8959c84c6c19bf18140cce78
willian95 <rodriguezwillian95@gmail.com>
2021-10-02 20:48:28 -0400

    rep

--- app/Console/Commands/RepJobExport.php
+++ app/Console/Commands/RepJobExport.php
@@ -138,8 +138,8 @@ class RepJobExport extends Command
 
             if(strpos($file, $id) > -1){
                 //dump(public_path()."/".$file);
-                dump("zip -r /var/www/psuv/public/".$id.".zip ".public_path()."/".$file);
-                exec("zip -r /var/www/psuv/public/".$id.".zip ".public_path()."/".$file);
+                dump("zip -r /var/www/psuv/public/".$id.".zip ".$file);
+                exec("zip -r /var/www/psuv/public/".$id.".zip ".$file);
 
             }
 
