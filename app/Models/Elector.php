38a84af655c28da47b37125343400449483d9b40
Sabas Vega <sabascarloseduardo@gmail.com>
2021-10-06 10:18:54 -0500

    setting in accessor full name

--- app/Models/Elector.php
+++ app/Models/Elector.php
@@ -16,15 +16,15 @@ class Elector extends Model
     public function getFullNameAttribute()
     {
         $name = $this->primer_nombre;
-        if (!empty($this->segundo_nombre)) {
-            $name .= ' ' .$this->segundo_nombre;
-        }
+        // if (!empty($this->segundo_nombre)) {
+        //     $name .= ' ' .$this->segundo_nombre;
+        // }
         if (!empty($this->primer_apellido)) {
             $name .= ' ' .$this->primer_apellido;
         }
-        if (!empty($this->segundo_apellido)) {
-            $name .= ' ' .$this->segundo_apellido;
-        }
+        // if (!empty($this->segundo_apellido)) {
+        //     $name .= ' ' .$this->segundo_apellido;
+        // }
         return $name;
     }
 
--- app/Models/PersonalCaracterizacion.php
+++ app/Models/PersonalCaracterizacion.php
@@ -37,15 +37,15 @@ class PersonalCaracterizacion extends Model
     public function getFullNameAttribute()
     {
         $name = $this->primer_nombre;
-        if (!empty($this->segundo_nombre)) {
-            $name .= ' ' .$this->segundo_nombre;
-        }
+        // if (!empty($this->segundo_nombre)) {
+        //     $name .= ' ' .$this->segundo_nombre;
+        // }
         if (!empty($this->primer_apellido)) {
             $name .= ' ' .$this->primer_apellido;
         }
-        if (!empty($this->segundo_apellido)) {
-            $name .= ' ' .$this->segundo_apellido;
-        }
+        // if (!empty($this->segundo_apellido)) {
+        //     $name .= ' ' .$this->segundo_apellido;
+        // }
         return $name;
     }
 
