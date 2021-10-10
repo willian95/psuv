8dca62a4fb76a5eaf9fc06f0bac5943e5a50ca62
willian95 <rodriguezwillian95@gmail.com>
2021-10-10 16:17:06 -0400

    reporte

--- app/Http/Controllers/Api/Reportes/ReporteCargaController.php
+++ app/Http/Controllers/Api/Reportes/ReporteCargaController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\CentroVotacion;
 use App\Models\MetasUbch;
 use App\Models\PersonalCaracterizacion;
-
+use Rap2hpoutre\FastExcel\FastExcel;
 
 class ReporteCargaController extends Controller
 {
@@ -77,6 +77,78 @@ class ReporteCargaController extends Controller
         $centroVotacionMetas = CentroVotacion::with("metasUbchs", "personalCaracterizacions",  "parroquia", "parroquia.municipio")->orderBy("nombre")->get();
 
         return ["metas" => $metas, "personalCaracterizacion" => $personalCaracterizacion, "centroVotacionMetas" => $centroVotacionMetas];
+    
+    }
+
+    function download(Request $request){
+
+        ini_set("memory_limit", -1);
+
+        $data = null;
+
+        if($request->centroVotacion != "0"){
+     
+            $data = $this->selectedCentroVotacionDownload($request->centroVotacion);
+        }
+        else if($request->parroquia != "0"){
+            
+            $data = $this->selectedParroquiaDownload($request->parroquia);
+        }
+        else if($request->municipio != "0"){
+      
+            $data = $this->selectedMunicipioDownload($request->municipio);
+        }  
+        else if($request->municipio == "0"){
+            
+            $data = $this->selectedAllDownload();
+        }  
+
+        return  (new FastExcel($data))->download('ReporteCarga.xlsx', function ($meta) {
+            
+            return [
+                'MUNICIPIO' => $meta->parroquia->municipio->nombre,
+                'PARROQUIA' => $meta->parroquia->nombre,
+                'CENTRO VOTACION' => $meta->nombre,
+                'META' => $meta->metasUbchs[0]->meta,
+                'CARGA' => count($meta->personalCaracterizacions),
+                'PENDIENTE' => $meta->metasUbchs[0]->meta - count($meta->personalCaracterizacions)
+            ];
+        });
+
+    }
+
+    function selectedCentroVotacionDownload($centroVotacion){
+
+        $centroVotacionMetas = CentroVotacion::where("id", $centroVotacion)->with("metasUbchs", "personalCaracterizacions", "parroquia", "parroquia.municipio")->orderBy("nombre")->get();
+
+        return$centroVotacionMetas;
+
+    }
+
+    function selectedParroquiaDownload($parroquia){
+        
+
+        $centroVotacionMetas = CentroVotacion::where("parroquia_id", $parroquia)->with("metasUbchs", "personalCaracterizacions", "parroquia", "parroquia.municipio")->orderBy("nombre")->get();
+
+        return $centroVotacionMetas;
+
+    }
+
+    function selectedMunicipioDownload($municipio){
+
+        $centroVotacionMetas = CentroVotacion::with("metasUbchs", "personalCaracterizacions", "parroquia", "parroquia.municipio")->whereHas("parroquia", function($q) use($municipio){
+            $q->where("municipio_id", $municipio);  
+        })->orderBy("nombre")->get();
+
+        return $centroVotacionMetas;
+    
+    }
+
+    function selectedAllDownload(){
+
+        $centroVotacionMetas = CentroVotacion::with("metasUbchs", "personalCaracterizacions",  "parroquia", "parroquia.municipio")->orderBy("nombre")->get();
+
+        return $centroVotacionMetas;
     
     }
 
