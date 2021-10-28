bde650c53f29b0a51c15846b38d00b03f4fbb2aa
willian95 <rodriguezwillian95@gmail.com>
2021-10-28 11:32:55 -0400

    migraciones

--- database/migrations/2021_10_28_101214_add_responsable_to_institucion.php
+++ database/migrations/2021_10_28_101214_add_responsable_to_institucion.php
@@ -14,7 +14,7 @@ class AddResponsableToInstitucion extends Migration
     public function up()
     {
         Schema::table('institucion', function (Blueprint $table) {
-            $table->string('responsable', 400);
+            $table->string('responsable', 400)->nullable();
         });
     }
 
