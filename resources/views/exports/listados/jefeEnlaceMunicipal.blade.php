9f2a74c85d79026c5c7e38b741d0eda970b6b6e5
willian rodriguez <rodriguezwillian95@gmail.com>
2022-04-03 14:49:08 -0400

    cambios

--- resources/views/exports/listados/jefeEnlaceMunicipal.blade.php
+++ resources/views/exports/listados/jefeEnlaceMunicipal.blade.php
@@ -2,7 +2,7 @@
     <thead>
         <tr>
             <td colspan="1">
-                <img src="{{ public_path('gob-logo.jpg') }}" alt="" style="width: 150px;">
+                <img src="{{ public_path('gob-logo.jpg') }}" alt="" style="width: 80px;">
             </td>
             <td colspan="4">
                 <h3 style="text-align:center;">Gobernación del Estado Falcón</h3>
