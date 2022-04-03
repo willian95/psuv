eee37742a670144859428f078eca228e31a70bdb
willian rodriguez <rodriguezwillian95@gmail.com>
2022-04-03 14:32:13 -0400

    cambios

--- app/Exports/listados/JefeEnlaceMunicipal.php
+++ app/Exports/listados/JefeEnlaceMunicipal.php
@@ -30,7 +30,14 @@ class JefeEnlaceMunicipal implements FromView
         WHERE rju.deleted_at is null ".$this->condition."
         order by municipio, parroquia, cedula;");
 
-        dump($data);
+        dump("SELECT mu.nombre municipio, pa.nombre parroquia, cv.nombre ubch, cedula, nombre_apellido, telefono_principal
+        FROM public_original.raas_jefe_ubch rju
+        left join public_original.raas_centro_votacion cv on cv.id=rju.raas_centro_votacion_id
+        left join public_original.raas_personal_caracterizacion pc on pc.id=rju.raas_personal_caracterizacion_id
+        left join public_original.raas_parroquia pa on pa.id=cv.raas_parroquia_id
+        left join public_original.raas_municipio mu on mu.id=pa.raas_municipio_id
+        WHERE rju.deleted_at is null ".$this->condition."
+        order by municipio, parroquia, cedula;");
 
         return view('exports.listados.jefeEnlaceMunicipal', ["data" => $data]);
     }
