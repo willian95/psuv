c7508fe8472f2cdc6cdb16ca9d4700398f57e0b9
willian rodriguez <rodriguezwillian95@gmail.com>
2022-05-13 11:36:31 -0400

    cambios

--- app/Traits/ElectorTrait.php
+++ app/Traits/ElectorTrait.php
@@ -141,7 +141,8 @@ trait ElectorTrait
      
             $response = Http::withHeaders([
                 'Content-Type' => 'text/html; charset=UTF-8',
-                'Accept' => "*"
+                'Accept' => "*",
+                'User-Agent' => "PostmanRuntime/7.29.0"
             ])->get('http://www.cne.gob.ve/web/registro_electoral/ce.php?nacionalidad='.$nacionalidad.'&cedula='.$cedula);
             $response = $response->body();
             $body = explode('<td', $response);
